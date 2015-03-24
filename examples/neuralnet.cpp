552bef92ca908e462f235bb89f6bb84e408bb6b8
Naum Azeredo <naumazeredo@gmail.com>
2015-03-23 22:39:31 -0300

    Modified neural net example to get multiple inputs

--- examples/neuralnet.cpp
+++ examples/neuralnet.cpp
@@ -1,10 +1,10 @@
 #include <cstdio>
 
-#include "neuralnet/neuralnet.h"
+#include "neuralnet/fnn.h"
 
 int main()
 {
-  nn::NeuralNet net;
+  nn::Feedforward net;
   net.setInputSize(1);
   net.addLayer(4);
   net.addLayer(2);
@@ -13,18 +13,17 @@ int main()
 
   std::vector<float> inputs;
 
-  for (int i = 0; i < 3; ++i)
+  float f;
+  while (scanf("%f", &f) != EOF)
   {
-    float f;
-    scanf("%f", &f);
+    inputs.clear();
     inputs.push_back(f);
+    inputs = net.activate(inputs);
+
+    for (unsigned int i = 0; i < inputs.size(); ++i)
+      printf("%f ", inputs[i]);
+    printf("\n");
   }
-
-  inputs = net.activate(inputs);
-
-  for (unsigned int i = 0; i < inputs.size(); ++i)
-    printf("%f ", inputs[i]);
-  printf("\n");
 
   return 0;
 }
