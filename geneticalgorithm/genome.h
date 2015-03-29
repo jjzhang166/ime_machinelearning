198aa0cd228e29dd015a551b17698fca625e586e
Naum Azeredo <naumazeredo@gmail.com>
2015-03-29 18:18:11 -0300

    Updated Genome documentation

--- geneticalgorithm/genome.h
+++ geneticalgorithm/genome.h
@@ -5,7 +5,7 @@ namespace ga
 {
 
 /**
- * Genome base class.
+ * Base class for genomes.
  * Genomes (usually) share this interface and basic attributes.
  * This is not used for polymorphism! GeneticAlgorithm works
  * with templates, new Genomes can be created simply by sharing
@@ -17,17 +17,17 @@ class Genome
 public:
   /**
    * Default constructor.
-   * Instantiate all to zero
+   * Instantiate all to zero.
    */
   Genome() : chromo_length_{0}, fitness_{0.0}
   {}
 
   /**
    * Explicit constructor.
-   * Necessary for GeneticAlgorithm genome creating.
+   * Necessary for GeneticAlgorithm genome creation.
    * Sets chromossome length and resets fitness to zero.
    * \param chromo_len Chromossome length (initial, in case of a variable size
-   *                   chromossome
+   *                   chromossome.
    */
   explicit Genome(unsigned chromo_len) : chromo_length_{chromo_len}, fitness_{0.0}
   {}
@@ -35,14 +35,14 @@ public:
   /**
    * Fitness setter.
    * Sets fitness value.
-   * \param fitness New fitness value
+   * \param fitness New fitness value.
    */
   void setFitness(double fitness) { fitness_ = fitness; }
 
   /**
    * Fitness getter.
    * Returns genome fitness.
-   * \return Fitness value
+   * \return Fitness value.
    */
   double getFitness() const { return fitness_; }
 
